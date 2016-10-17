e03dc37a7c169a846cb5e50bcfe3c0b7b3e36e88
lipann <lipann@yandex.ru>
2016-10-18 01:10:21 +0300

    fix bug with file and add unbalanced choice

--- 02/launch.py
+++ 02/launch.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 try:
     from termcolor import colored
@@ -13,83 +14,101 @@ def PrintTime(start):
         print(colored("Time: {0} s".format(time.time() - start), "magenta"))
     except Exception:
         print("Time: {0} s".format(time.time() - start))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_size",
+                        help="""set train dataset size as len(data) * TRAIN_SIZE.
+                        By default TRAIN_SIZE=0.6""",
+                        type=float)
+    
+    parser.add_argument("--shuffle",
+                        help="shuffle dataset before choosing samples",
+                        action="store_true")
+    
+    parser.add_argument("--balanced",
+                        help ="""get balanced dataset with an equal number of 
+                        samples in each classes. By default train dataset will 
+                        be unbalanced, at least with one sample from each class""",
+                        action="store_true")    
         
-def PrintHelp():
-    print("print --shuffle [train_size_percent=0.6] for shuffling data and get train dataset with size = len(data) * train_size_percent. Other samples will become a test dataset")
-    print("then script will make project, launch it: fit on trainig set and predict on testing set and then compare predictions with real classes")
-
-
-if "--help" in sys.argv or "-h" in sys.argv:
-    PrintHelp()
-    sys.exit(0)
+    args = parser.parse_args()
+    train_size = "" if not args.train_size else "--train_size {0}".format(args.train_size)
+    shuffle = "" if not args.shuffle else "--shuffle"
+    balanced = "" if not args.balanced else "--balanced"
     
-if "--shuffle" in sys.argv:
     start = time.time()
-    if len(sys.argv) == 3:
-        os.system("python3 data/multiclass/split_data.py {0}".format(sys.argv[2]))
+    if os.system("python3 data/multiclass/split_data.py {0} {1} {2}".format(train_size, shuffle, balanced)) == 0:
+        try:
+            print(colored("Generating train and test datasets done", "green"))
+        except Exception:
+            print("Generating train and test datasets done")
+        PrintTime(start)
     else:
-        os.system("python3 data/multiclass/split_data.py")
-    try:
-        print(colored("Shuffle done", "green"))
-    except Exception:
-        print("Suffle done")
-    PrintTime(start)
+        try:
+            print(colored("Generating train and test datasets failed", "red"))
+        except Exception:
+            print("Generating train and test datasets failed")
+        sys.exit(0)        
+        
+    start = time.time()
+    if os.system("make -C ./template") == 0:
+        try:
+            print(colored("Make done", "green"))
+        except Exception:
+            print("Make done")
+        PrintTime(start)
+    else:
+        try:
+            print(colored("Make failed", "red"))
+        except Exception:
+            print("Make failed")
+        sys.exit(0)
+        
+    start = time.time()        
+    if os.system("./template/build/bin/task2 -d ./data/multiclass/train_labels.txt -m ./template/build/bin/model.txt --train") == 0:
+        try:
+            print(colored("Fit done", "green"))
+        except:
+            print("Fit done")
+        PrintTime(start)
+    else:
+        try:
+            print(colored("Fit failed", "red"))
+        except Exception:
+            print("Fit failed")
+        sys.exit(0)
+        
+    start = time.time()        
+    if os.system("cd ./template/build/bin/ && ./task2 -d ../../../data/multiclass/test_labels.txt -m model.txt -l predictions.txt --predict") == 0:
+        try:
+            print(colored("Predict done", "green"))
+        except Exception:
+            print("Predict done")
+        PrintTime(start)
+    else:
+        try:
+            print(colored("Predict failed", "red"))
+        except Exception:
+            print("Predict failed")
+        sys.exit(0)
+        
     
-start = time.time()
-if os.system("make -C ./template") == 0:
-    try:
-        print(colored("Make done", "green"))
-    except Exception:
-        print("Make done")
-    PrintTime(start)
-else:
-    try:
-        print(colored("Make failed", "red"))
-    except Exception:
-        print("Make failed")
-    sys.exit(0)
-    
-start = time.time()        
-if os.system("./template/build/bin/task2 -d ./data/multiclass/train_labels.txt -m ./template/build/bin/model.txt --train") == 0:
-    try:
-        print(colored("Fit done", "green"))
-    except:
-        print("Fit done")
-    PrintTime(start)
-else:
-    try:
-        print(colored("Fit failed", "red"))
-    except Exception:
-        print("Fit failed")
-    sys.exit(0)
-    
-start = time.time()        
-if os.system("cd ./template/build/bin/ && ./task2 -d ../../../data/multiclass/test_labels.txt -m model.txt -l predictions.txt --predict") == 0:
-    try:
-        print(colored("Predict done", "green"))
-    except Exception:
-        print("Predict done")
-    PrintTime(start)
-else:
-    try:
-        print(colored("Predict failed", "red"))
-    except Exception:
-        print("Predict failed")
-    sys.exit(0)
-    
-
-start = time.time()        
-if os.system("cd ./template && python ./compare.py ../data/multiclass/test_labels.txt ./build/bin/predictions.txt") == 0:
-    try:
-        print(colored("Comparation done", "green"))
-    except Exception:
-        print("Comparation done")
-    PrintTime(start)
-else:
-    try:
-        print(colored("Comparation failed", "red"))
-    except Exception:
-        print("Comparation failed")
-    sys.exit(0)
+    start = time.time()        
+    if os.system("cd ./template && python ./compare.py ../data/multiclass/test_labels.txt ./build/bin/predictions.txt") == 0:
+        try:
+            print(colored("Comparation done", "green"))
+        except Exception:
+            print("Comparation done")
+        PrintTime(start)
+    else:
+        try:
+            print(colored("Comparation failed", "red"))
+        except Exception:
+            print("Comparation failed")
+        sys.exit(0)
+        
+main()
 
         
--- 02/split_data.py
+++ 02/split_data.py
@@ -1,45 +1,84 @@
 import os, sys
-from random import shuffle
+import random
+import argparse
 
-train_size = 0.6
-if len(sys.argv) > 1:
-    train_size = float(sys.argv[1])
+def main():
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_size",
+                        help="""set train dataset size as len(data) * TRAIN_SIZE.
+                        By default TRAIN_SIZE=0.6""",
+                        type=float)
     
-txt_files = list(filter(lambda x : x.endswith(".txt"), os.listdir("./data/multiclass")))
-
-if len(txt_files) == 1:
-    os.system("mv ./data/multiclass/{0}".format(txt_files[0]) + " ./data/multiclass/all_pictures.txt")
-
-fin = open("./data/multiclass/all_pictures.txt")
-pictures_and_labels = [i.strip().split() for i in fin.readlines()]
+    parser.add_argument("--shuffle",
+                        help="shuffle dataset before choosing samples",
+                        action="store_true")
     
-classes = dict()
-for (path, label) in pictures_and_labels:
-    if label not in classes:
-        classes[label] = [path]
-    else:
-        classes[label].append(path)
-
-for key in classes:
-    shuffle(classes[key])
-
-train_pictures = []
-test_pictures = []
-for key in classes:
-    size = len(classes[key])
-    train_pictures.append((key, classes[key][:int(size * train_size)]))
-    test_pictures.append((key, classes[key][int(size * train_size):]))
-    
-fout = open("./data/multiclass/train_labels.txt", "w")
-for (label, paths) in train_pictures:
-    for path in paths:
-        print(path, label, file=fout)
+    parser.add_argument("--balanced",
+                        help ="""get balanced dataset with an equal number of 
+                        samples in each classes. By default train dataset will 
+                        be unbalanced, at least with one sample from each class""",
+                        action="store_true")    
         
-fout.close()
-fout = open("./data/multiclass/test_labels.txt", "w")
-
-for (label, paths) in test_pictures:
-    for path in paths:
+    args = parser.parse_args()
+    train_size = 0.6 if not args.train_size else args.train_size
+    shuffle = args.shuffle
+    balanced = args.balanced
+    
+    txt_files = list(filter(lambda x : x.endswith(".txt"), os.listdir("./data/multiclass")))
+    
+    if len(txt_files) == 1:
+        os.system("mv ./data/multiclass/{0}".format(txt_files[0]) + " ./data/multiclass/all_pictures.txt")
+    
+    fin = open("./data/multiclass/all_pictures.txt")
+    pictures_and_labels = [i.strip().split() for i in fin.readlines()]
+        
+    classes = dict()
+    for (path, label) in pictures_and_labels:
+        if label not in classes:
+            classes[label] = [path]
+        else:
+            classes[label].append(path)
+    
+    if shuffle:
+        for key in classes:
+            random.shuffle(classes[key])
+    
+    train_pictures = []
+    test_pictures = []
+    if balanced:
+        for key in classes:
+            size = len(classes[key])
+            for path in classes[key][:int(size * train_size)]:
+                train_pictures.append((key, path))
+            for path in classes[key][int(size * train_size):]:
+                test_pictures.append((key, path))
+        random.shuffle(train_pictures)
+        random.shuffle(test_pictures)
+    else:
+        almost_all = []
+        
+        for key in classes:
+            train_pictures.append((key, classes[key][0]))
+            for path in classes[key][1:]:
+                almost_all.append((key, path))
+                
+        random.shuffle(almost_all)
+        size = int(len(almost_all) * train_size)
+        train_pictures += almost_all[:size]
+        test_pictures += almost_all[size:]
+            
+        
+    fout = open("./data/multiclass/train_labels.txt", "w")
+    for (label, path) in train_pictures:
         print(path, label, file=fout)
-
-fout.close()
+            
+    fout.close()
+    fout = open("./data/multiclass/test_labels.txt", "w")
+    
+    for (label, path) in test_pictures:
+        print(path, label, file=fout)
+    
+    fout.close()
+    
+main()
